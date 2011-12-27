42e52ae41dfed9ef5c1e839805ef93a04a8b1254
Craig Martin <cmgecola@gmail.com>
2011-12-27 11:14:20 -0600

    Modified desktop icon positions. Removed 'My Documents' for now.

--- desktopicons.php
+++ desktopicons.php
@@ -1,14 +1,15 @@
 <nav class="two columns container">
-	<a href="/"><figure>
+	<a class="row" href="/"><figure class="desktop-icon">
 		<img src="images/go-home.png" />
 		<figcaption>Home</figcaption>
 	</figure></a>
-	<a href="/"><figure>
+	<!--
+	<a class="row" href="/"><figure class="desktop-icon">
 		<img src="images/document-open.png" />
 		<figcaption>My Documents</figcaption>
-	</figure></a>
-	<a href="/intro.php" title="Introduction.php"><figure>
-		<img src="images/document-new.png" alt="Introduction.php"/>
-		<figcaption>Introduction.php</figcaption>
+	</figure></a>-->
+	<a class="row" href="/intro.php" title="Intro.php"><figure>
+		<img src="images/document-new.png" alt="Intro.php"/>
+		<figcaption>Intro.php</figcaption>
 	</figure></a>
 </nav>
