6f5805a73c84940c01f7e23bb76b312e270d4817
Craig Martin <cmgecola@gmail.com>
2011-12-27 11:13:47 -0600

    Copied the License file in to a copyright.php to format it nicely.

---
+++ copyright.php
@@ -0,0 +1,44 @@
+<?php include("header.php"); ?>
+	
+	<section class="row">
+		<?php include("desktopicons.php"); ?>
+		
+		<section class="eight columns">
+			<header class="row graybar rounded-corners">
+				<div class="twelve columns">
+					<hgroup class="left container">
+						<h3>Copyright</h3>
+					</hgroup>
+					
+					<?php include("windowButtons.php"); ?>
+				</div>
+			</header>
+			<section class="row application">
+				<article class="eleven columns container">
+					<p> 
+						Copyright (C) 2010-2011 Craig Martin
+						<br/><br/>
+						Permission is hereby granted, free of charge, to any person obtaining a copy of<br/>
+						this software and associated documentation files (the "Software"), to deal in<br/>
+						the Software without restriction, including without limitation the rights to<br/>
+						use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies<br/>
+						of the Software, and to permit persons to whom the Software is furnished to do<br/>
+						so, subject to the following conditions:<br/><br/>
+						
+						The above copyright notice and this permission notice shall be included in all<br/>
+						copies or substantial portions of the Software.<br/><br/>
+						
+						THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br/>
+						IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,<br/>
+						FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE<br/>
+						AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER<br/>
+						LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,<br/>
+						OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE<br/>
+						SOFTWARE.
+					</p>
+				</article>
+			</section>
+		</section>
+	</section>
+	
+<?php include("footer.php"); ?>
