32a299a947ae068cfdd7570c0bd4a2c7e995750b
Craig Martin <cmgecola@gmail.com>
2011-12-29 19:49:11 -0600

    Add new content

---
+++ craig.php
@@ -0,0 +1,39 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/header.php'); ?>
+	
+	<section class="row">
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/desktopicons.php'); ?>
+		
+		<section class="eight columns">
+			<header class="row graybar rounded-corners">
+				<div class="twelve columns">
+					<hgroup class="left container">
+						<h3>Craig</h3>
+					</hgroup>
+					
+					<?php include($_SERVER['DOCUMENT_ROOT'].'/windowButtons.php'); ?>
+				</div>
+			</header>
+			<section class="row application">
+				<div class="twelve columns">
+					<ul class="row">
+						<li class="three columns">
+							<a href="/craig/about.php" title="about.php"><figure>
+								<img src="/images/document-new.png" alt="about.php"/>
+								<figcaption>About.php</figcaption>
+							</figure></a>
+						</li>
+						<li class="three columns">
+							<a href="craig/contact.php" title="contact.php"><figure>
+								<img src="/images/document-new.png" alt="contact.php"/>
+								<figcaption>Contact.php</figcaption>
+							</figure></a>
+						</li>
+						<li class="three columns"></li>
+						<li class="three columns"></li>
+					</ul>
+				</div>
+			</section>
+		</section>
+	</section>
+	
+<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.php'); ?>
