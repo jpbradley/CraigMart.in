99828f5617ad62dc754bee67a0064873f838ed84
Craig Martin <cmgecola@gmail.com>
2011-12-29 20:11:45 -0600

    Updated content for About

--- craig/about.php
+++ craig/about.php
@@ -7,14 +7,31 @@
 			<header class="row graybar rounded-corners">
 				<div class="twelve columns">
 					<hgroup class="left container">
-						<h3></h3>
+						<h3>About</h3>
 					</hgroup>
 					
 					<?php include($_SERVER['DOCUMENT_ROOT'].'/windowButtons.php'); ?>
 				</div>
 			</header>
 			<section class="row application">
-			
+				<article class="four columns container">
+					<img src="/images/profile.png" />
+				</article>
+				<article class="seven columns">
+					<p>
+						Craig Martin is a recent graduate where he earned a Bachelors Degree in Electrical Engineering 
+						with two minors in Computer Science and Physics.<br/>
+						Growing up, he always loved to tinker with things and learn how stuff worked. That passion is still 
+						with him and is the reason why he pursued an Engineering degree.<br/><br/>
+						Now, he enjoys creating projects for himself while he is not at work using the knowledge that he has 
+						gained from school. His projects mainly focus on either software or firmware. For example, building this 
+						website or making a line following robot.<br/><br/>
+						Additionally, creating these projects is a great way for him to learn on his own. It allows him to be 
+						challenged and introduced to new and better things. Even though he may be re-inventing the wheel with 
+						his projects, he believes that it is important to try things on his own so he has a deeper understanding of 
+						basic concepts that will enable him to apply the concepts to a new idea.
+					</p>
+				</article>
 			</section>
 		</section>
 	</section>
