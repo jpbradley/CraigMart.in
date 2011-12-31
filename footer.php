d2f63a039256e17be78dd10b77f58a3fde073968
Craig Martin <cmgecola@gmail.com>
2011-12-31 11:12:43 -0600

    Fixed footer grid

--- footer.php
+++ footer.php
@@ -1,25 +1,28 @@
-	</div>
+	</div><!-- #wrapper  From header -->
 	<!-- It would be nice if this was at the very bottom of the screen at all times -->
-	<footer class="row">
-		<div class="twelve columns">
-			<section class="left container">
-				<a href="/copyright.php" title="Copyright">
-					<figure>
-						<img src="/images/document-new.png" alt="Copyright"/>
-						<figcaption>Copyright</figcaption>
-					</figure>
-				</a>
-				<!-- Copyright &copy; 2010-2011 Craig Martin -->
-			</section>
-			
-			<section class="right container">
-				<a href="#" title="Archive">
-					<figure>
-						<img src="/images/recycle.png" alt="Archive" />
-						<figcaption>Archive</figcaption>
-					</figure>
-				</a>
-			</section>
+	<footer>
+		<div class="container">
+		<nav class="container">
+			<ul class="row">
+				<li class="one columns">
+					<a href="/copyright.php" title="Copyright">
+						<figure>
+							<img src="/images/document-new.png" alt="Copyright"/>
+							<figcaption>Copyright</figcaption>
+						</figure>
+					</a>
+				</li>
+				<li class="ten columns"></li>
+				<li class="one columns">
+					<a href="#" title="Archive">
+						<figure>
+							<img src="/images/recycle.png" alt="Archive" />
+							<figcaption>Archive</figcaption>
+						</figure>
+					</a>
+				</li>
+			</ul>
+		</nav>
 		</div>
 	</footer>
 </body>
