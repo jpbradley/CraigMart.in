3722877501b56ba22432d5b2fea95ef19384bf36
Craig Martin <cmgecola@gmail.com>
2011-12-31 11:19:28 -0600

    Removed link for Maximize

--- windowButtons.php
+++ windowButtons.php
@@ -1,5 +1,5 @@
 <nav class="right" style="padding-top: 15px;">
 	<a class="container" href="#" title="Minimize"><img src="/images/minimize.png" alt="Minimize"/></a>
-	<a href="#" title="Read More"><img src="/images/square.png" alt="Read More"/></a>
+	<img src="/images/square.png" alt="Read More"/>
 	<a class="container" href="/desktop.php" title="Home"><img src="/images/close.png" alt="Home"/></a>
 </nav>
