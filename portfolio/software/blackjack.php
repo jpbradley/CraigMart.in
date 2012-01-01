09c35039db6d51cee296e629028185bfb3a791ff
Craig Martin <cmgecola@gmail.com>
2012-01-01 14:51:26 -0600

    Add padding to bottom of Blackjack.php

--- portfolio/software/blackjack.php
+++ portfolio/software/blackjack.php
@@ -48,7 +48,7 @@
 					because &ldquo;If it ain&rsquo;t broke it doesn&rsquo;t have enough features.&rdquo; 
 					Some of those additional features included a GUI for the client, parallel betting, double down, 
 					and stat tracking.</p>
-				<p><img alt="" src="/images/portfolio/software/blackjack/bjtable.png"/></p>
+				<p><img alt="Blackjack GUI" src="/images/portfolio/software/blackjack/bjtable.png"/></p>
 				<p>After all of the coding was finished, we were able to host a server on one computer and 
 					have the clients connect to the host from any other computer (within the school network because of firewalls).</p>
 				<p>The game functions well and sometimes can be frustrating because it feels like the dealer cheats.</p>
@@ -58,7 +58,10 @@
 					Download and install <a href="http://www.microsoft.com/sqlserver/en/us/editions/express.aspx" target="_blank">SQL Server Express</a><br />
 					SQL server will need to be configured to allow <a href="http://msdn.microsoft.com/en-us/library/bb909712%28v=vs.90%29.aspx" target="_blank">TCP/IP ports</a><br />
 					Finally, the server, client, and SQL scripts can be found at our <a href="http://code.google.com/p/cisc370-finalproject/downloads/list" target="_blank">Google code page</a><br />
-					For more information about the project, you can look around the our <a href="http://code.google.com/p/cisc370-finalproject/" target="_blank">Google code page.</a></p>
+					For more information about the project, you can look around the our <a href="http://code.google.com/p/cisc370-finalproject/" target="_blank">Google code page.</a>
+					
+				</p>
+				<br/>
 				</article>
 			</section>
 		</section>
