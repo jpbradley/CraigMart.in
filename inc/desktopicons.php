0c0cbda62c08961927c202aed7d46c7d9327e1bb
Craig Martin <cmgecola@gmail.com>
2012-01-05 19:53:07 -0600

    Removed Guides. Going to use www.CraigRoyMartin.com for blog/guides

--- inc/desktopicons.php
+++ inc/desktopicons.php
@@ -7,10 +7,6 @@
 		<img src="/images/document-open.png" alt="Portfolio" />
 		<figcaption>Portfolio</figcaption>
 	</figure></a>
-	<a class="row" href="/index.php?p=guides" title="Guides"><figure class="desktop-icon">
-		<img src="/images/document-open.png" alt="Guides" />
-		<figcaption>Guides</figcaption>
-	</figure></a>
 	<a class="row" href="/" title="Intro.php"><figure>
 		<img src="/images/document-new.png" alt="Intro.php"/>
 		<figcaption>Intro.php</figcaption>
